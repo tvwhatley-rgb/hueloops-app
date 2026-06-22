102aedad2692f63756f095fc94e3291937cfb453
tvwhatley-rgb <tvwhatley@gmail.com>
2026-06-22 18:46:42 -0500

    added permissions

--- android/app/src/main/java/com/hueloops/app/MainActivity.java
+++ android/app/src/main/java/com/hueloops/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.hueloops.app;
-
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Retrieve the native Android WebView and customize permissions
+        WebView webView = this.getBridge().getWebView();
+        WebSettings webSettings = webView.getSettings();
+        
+        // Force-enable autoplay for custom audio elements and blobs
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+    }
+}
