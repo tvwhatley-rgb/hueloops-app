fba191fe921cfe6e8624df31c89bb670c3b42356
tvwhatley-rgb <tvwhatley@gmail.com>
2026-06-23 09:00:46 -0500

    Fixing crash issue

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,7 +8,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Core Fix: Initialize Firebase on native startup before any Google/Apple plugins execute
+        FirebaseApp.configure()
         return true
     }
 
